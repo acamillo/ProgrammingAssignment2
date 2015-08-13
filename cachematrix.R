62c596ac5e9a323937e38123507295c60ed23e6d
acamillo <camillo.alessandro@gmail.com>
2015-08-13 16:16:18 +0200

    First drop

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,56 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function transform an R matrix into an augmented object
+## capable of holding private properties. In also
+## provides object mutators and getters.
 
+## This matrix store the inverse within its private attributes inv_matrix
+## whose access is regulamentead by setInverse() and getInverse() 
+## methods.
+makeCacheMatrix <- function(x = matrix()) {
+    inv_matrix<-NULL
+    
+    set<-function(y)
+    {
+        x<<-y
+        inv_matrix<<-NULL
+    }
+    
+    get<-function() x
+    
+    setInverse <- function(inv) inv_matrix <<- inv
+    
+    getInverse <- function() inv_matrix
+    
+    list(set=set, get=get,
+         setInverse=setInverse,
+         getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the augumented "matrix"
+## objects type, produced by the makeCacheMatrix function.
+## The inverse matrix is store within a private object property
+## so to speed up the inverse matrix computation.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The function accesses the private property of the matrix using
+## the provided getters and setter methods.
+
+## If you call twice the cacheSolve() function, you will see a
+## "getting cached data" message printed to the console.
+
+cacheSolve <- function(x=matrix(), ...) {
+    
+    inv <- x$getInverse()
+    
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    
+    matrix<-x$get()
+    inv<-solve(matrix, ...)
+    x$setInverse(inv)
+    inv
 }
